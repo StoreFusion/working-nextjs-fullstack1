fab8a0fe5a202f8e1ca0ad9467e0406b7677c8c1
StoreFusionMain <tdur@protonmail.com>
2023-11-25 18:59:55 +0530

    [update] ts chill

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,26 +6,26 @@ import values from '@/app/lib/placeholder-data'
 console.log("a simple check on status"); // next.js default comment (test) 
 console.log(values);
 
-function Pen(name, color, price) {
-  this.name = name;
-  this.color = color;
-  this.price = price;
-}
+// function Pen(name, color, price) {
+//   this.name = name;
+//   this.color = color;
+//   this.price = price;
+// }
 
-const pen1 = new Pen("Marker", "Blue", "$3");
+// const pen1 = new Pen("Marker", "Blue", "$3");
 
-class pencil {
-  constructor(nib: string, size: number){
-    this.nib = nib;
-    this.size = size; 
-  }
-  showNib() {
-    console.log(`the nib is ${this.nib}`);
-  }
-}
-console.log(pen1)
-const pencil1 = new pencil('graphite', 10);
-pencil1.showNib();
+// class pencil {
+//   constructor(nib: string, size: number){
+//     this.nib = nib;
+//     this.size = size; 
+//   }
+//   showNib() {
+//     console.log(`the nib is ${this.nib}`);
+//   }
+// }
+// console.log(pen1)
+// const pencil1 = new pencil('graphite', 10);
+// pencil1.showNib();
 
 export const metadata: Metadata = {
   title: 'Customers',
